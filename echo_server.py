d097875f1dfa247fdc2308a16c3c586c08f6fd6d
rjynn <rdejesus@cs.ualberta.ca>
2019-01-16 17:59:04 -0700

    pushing proxy stuff

--- echo_server.py
+++ echo_server.py
@@ -9,12 +9,16 @@ BUFFER_SIZE = 1024
 def main():
     #create socket 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        #allows to reuse same bind port
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)      
+
         s.bind((HOST, PORT))
         s.listen(1) #make socket listen
        
         #listen forever for connections
         while True:
             conn, addr = s.accept() #accept incoming connections
+            print(addr)
             full_data = b""
             while True:
                 data = conn.recv(BUFFER_SIZE)
